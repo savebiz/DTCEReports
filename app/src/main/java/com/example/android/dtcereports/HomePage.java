848d557f8202f1492c52bb9d5fac511277e5336c
adolphchris19 <adolphchris19@gmail.com>
2018-07-19 08:45:25 -0700

    Some changes

--- app/src/main/java/com/example/android/dtcereports/HomePage.java
+++ app/src/main/java/com/example/android/dtcereports/HomePage.java
@@ -2,6 +2,7 @@ package com.example.android.dtcereports;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -9,19 +10,32 @@ import android.widget.LinearLayout;
 
 public class HomePage extends AppCompatActivity {
     LinearLayout l1,l2;
-    Button btnsub;
+    Button button;
     Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        btnsub = (Button)findViewById(R.id.buttonsub);
+
+
+        //Onclick Listener To disPlay The next Activity
+        Button mainView = findViewById(R.id.button);
+        mainView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.content.Intent nextScreen = new android.content.Intent(HomePage.this, MainActivity.class);
+                startActivity(nextScreen);
+            }
+        });//End of onClick Listener
+
+        //Animation Effect
+        button = (Button)findViewById(R.id.button);
         l1 = (LinearLayout) findViewById(R.id.l1);
         l2 = (LinearLayout) findViewById(R.id.l2);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
         downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
         l1.setAnimation(uptodown);
-        l2.setAnimation(downtoup);
+        l2.setAnimation(downtoup);//End of Animation Effect
     }
 
 
